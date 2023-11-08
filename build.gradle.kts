5262a081e6894290e413acda0974c831dbc919ab
Riley Park <rileysebastianpark@gmail.com>
2023-11-08 10:49:03 -0800

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  val indraVersion = "3.1.2"
-  id("com.diffplug.spotless") version "6.20.0"
+  val indraVersion = "3.1.3"
+  id("com.diffplug.spotless") version "6.22.0"
   id("net.kyori.indra") version indraVersion
   id("net.kyori.indra.checkstyle") version indraVersion
   id("net.kyori.indra.publishing") version indraVersion
@@ -61,13 +61,13 @@ repositories {
 dependencies {
   annotationProcessor("ca.stellardrift:contract-validator:1.0.1")
   checkstyle("ca.stellardrift:stylecheck:0.2.1")
-  errorprone("com.google.errorprone:error_prone_core:2.21.1")
+  errorprone("com.google.errorprone:error_prone_core:2.23.0")
   compileOnlyApi("org.jetbrains:annotations:24.0.1")
   compileOnlyApi("org.jspecify:jspecify:0.3.0")
   api("com.google.inject:guice:7.0.0")
   api("com.google.inject.extensions:guice-assistedinject:7.0.0")
-  testImplementation("com.google.guava:guava-testlib:32.1.2-jre")
-  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("com.google.guava:guava-testlib:32.1.3-jre")
+  testImplementation(platform("org.junit:junit-bom:5.10.1"))
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
